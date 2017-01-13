f7f006cf16fe4b251f3d31acb155e72e6f282f39
justinl <justinl@etana.co.za>
2017-01-13 21:19:07 +0200

    -> Updated service worker

---
+++ src/js/initialiseFirebase.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBYwXYPC4BAZiRaFWNWeJ8GnVQA1wUiQlQ",
+    authDomain: "my-game-viewing-app.firebaseapp.com",
+    databaseURL: "https://my-game-viewing-app.firebaseio.com",
+    storageBucket: "my-game-viewing-app.appspot.com",
+    messagingSenderId: "949532524234"
+};
+firebase.initializeApp(config);
